6b665544ed1f2555195edee680bcafac521c13e9
Edgar Chen <echen@mozilla.com>
2015-04-30 19:58:16 +0800

    Bug 1159591 - Part 9: Move MMI logic from ril_worker to telephonyService (Call Waiting). r=aknow

--- dom/mobileconnection/gonk/MobileConnectionService.js
+++ dom/mobileconnection/gonk/MobileConnectionService.js
@@ -965,7 +965,9 @@ MobileConnectionProvider.prototype = {
         return false;
       }
 
-      aCallback.notifySuccessWithBoolean(aResponse.enabled);
+      aCallback.notifySuccessWithBoolean(
+        aResponse.enabled &&
+        (aResponse.serviceClass & RIL.ICC_SERVICE_CLASS_VOICE) == 0x01);
       return false;
     }).bind(this));
   },
